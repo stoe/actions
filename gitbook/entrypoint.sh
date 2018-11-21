22ab99f730020dd427ad5358bd2e917fbf45a059
stoe <stoe@github.com>
2018-11-21 16:05:08 +0100

    Add user details for git commit

--- gitbook/entrypoint.sh
+++ gitbook/entrypoint.sh
@@ -4,25 +4,39 @@ set -eu
 
 cd "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}"
 
-gitbook install 2> /dev/null
-gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
+GITBOOK_FILE_PATH="${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
+
+HAS_PDF_COMMIT=$(cat "${GITHUB_EVENT_PATH}" | jq .commits[0].added[0] | tr -d '"')
+
+if [ "./${HAS_PDF_COMMIT}" == "${GITBOOK_FILE_PATH}" ]; then
+  echo "==> skipping…"
+  exit 0;
+fi
+
+gitbook install
+
+echo
+echo "==> Generating ${GITBOOK_FILE_PATH}…"
+gitbook pdf ./ ${GITBOOK_FILE_PATH}
+echo "Done."
+echo
 
 cd "${GITHUB_WORKSPACE}"
 
-GIT_NAME=$("${GITHUB_EVENT_PATH}" | jq .pusher.name)
-GIT_EMAIL=$("${GITHUB_EVENT_PATH}" | jq .pusher.email)
+GIT_NAME=$(cat "${GITHUB_EVENT_PATH}" | jq .pusher.name | tr -d '"')
+GIT_EMAIL=$(cat "${GITHUB_EVENT_PATH}" | jq .pusher.email | tr -d '"')
 
-git config user.email "${GIT_EMAIL}"
 git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
 
 git status	# debug
 
-git add "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
+git add "${GITBOOK_FILE_PATH}"
 
 git status	# debug
 
-git commit -m "Add ${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
-
-git status	# debug
-
-# git push
+# git commit -m "Add ${GITBOOK_FILE_PATH}"
+#
+# git status	# debug
+#
+# # git push
