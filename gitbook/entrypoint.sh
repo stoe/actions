dc1ccb5e052eede7719e75c8612c42f555bbb579
Stefan Stölzle <stoe@github.com>
2018-11-21 15:08:15 +0100

    Commit book PDF

--- gitbook/entrypoint.sh
+++ gitbook/entrypoint.sh
@@ -4,11 +4,19 @@ set -eu
 
 cd "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}"
 
-# gitbook install 2> /dev/null
-# gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
+gitbook install 2> /dev/null
+gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
 
-cd ..
+cd "${GITHUB_WORKSPACE}"
 
-git status
-git add "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}*.pdf"
-git status
+git status	# debug
+
+git add "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
+
+git status	# debug
+
+git commit -m "Add ${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
+
+git status	# debug
+
+# git push
