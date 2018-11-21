3aa2ecb9b7ddeabdf8aa284c41f436c0baa5ff73
Stefan Stölzle <stoe@github.com>
2018-11-21 15:03:10 +0100

    Look for git installation

--- gitbook/entrypoint.sh
+++ gitbook/entrypoint.sh
@@ -4,7 +4,9 @@ set -eu
 
 cd "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}"
 
-gitbook install 2> /dev/null
-gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
+# gitbook install 2> /dev/null
+# gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
 
-ls -ltr
+cd ..
+
+git --version
