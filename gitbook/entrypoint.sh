acc567ca36849a843079d6d8a892c7446e71d4c2
stoe <stoe@github.com>
2018-11-21 16:37:15 +0100

    Use GITBOOK_PDF_NAME

--- gitbook/entrypoint.sh
+++ gitbook/entrypoint.sh
@@ -4,11 +4,12 @@ set -eu
 
 cd "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}"
 
-GITBOOK_FILE_PATH="${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
+GITBOOK_PDF_NAME="${GITBOOK_NAME:-book}.pdf"
+GITBOOK_FILE_PATH="${GITBOOK_FOLDER:-.}/${GITBOOK_PDF_NAME}"
 
 HAS_PDF_COMMIT=$(cat "${GITHUB_EVENT_PATH}" | jq .commits[0].added[0] | tr -d '"')
 
-if [ "./${HAS_PDF_COMMIT}" == "${GITBOOK_FILE_PATH}" ]; then
+if [ "${HAS_PDF_COMMIT}" == "${GITBOOK_FILE_PATH}" ]; then
   echo "==> skipping…"
   exit 0;
 fi
@@ -17,11 +18,9 @@ gitbook install
 
 echo
 echo "==> Generating ${GITBOOK_FILE_PATH}…"
-gitbook pdf ./ ${GITBOOK_FILE_PATH}
+gitbook pdf ./ ${GITBOOK_PDF_NAME}
 echo "Done."
 echo
-
-cd "${GITHUB_WORKSPACE}"
 
 GIT_NAME=$(cat "${GITHUB_EVENT_PATH}" | jq .pusher.name | tr -d '"')
 GIT_EMAIL=$(cat "${GITHUB_EVENT_PATH}" | jq .pusher.email | tr -d '"')
@@ -31,11 +30,11 @@ git config user.email "${GIT_EMAIL}"
 
 git status	# debug
 
-git add "${GITBOOK_FILE_PATH}"
-
-git status	# debug
-
-# git commit -m "Add ${GITBOOK_FILE_PATH}"
+# git add "${GITBOOK_PDF_NAME}"
+#
+# git status	# debug
+#
+# git commit -m "Add ${GITBOOK_PDF_NAME}"
 #
 # git status	# debug
 #
