c0057326bff14143eff835a15abd0dd499668f96
Stefan Stölzle <stoe@github.com>
2018-11-21 15:43:01 +0100

    Add user details for git commit

--- gitbook/entrypoint.sh
+++ gitbook/entrypoint.sh
@@ -9,6 +9,12 @@ gitbook pdf ./ ${GITBOOK_NAME:-book}.pdf
 
 cd "${GITHUB_WORKSPACE}"
 
+GIT_NAME=$("${GITHUB_EVENT_PATH}" | jq .pusher.name)
+GIT_EMAIL=$("${GITHUB_EVENT_PATH}" | jq .pusher.email)
+
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_NAME}"
+
 git status	# debug
 
 git add "${GITHUB_WORKSPACE}/${GITBOOK_FOLDER:-.}/${GITBOOK_NAME:-book}.pdf"
